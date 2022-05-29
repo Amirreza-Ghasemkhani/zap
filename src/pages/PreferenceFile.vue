dda71862dd59f54d6f94618ae23980c5606e6d8d
Amirreza Ghasemkhani <A.Ghasemkhaniii@gmail.com>
2022-05-30 01:27:01 +0430

    Add New Page In Preferences Section

---
+++ src/pages/PreferenceFile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class='text-h4 q-mb-md'>User File settings</div>
+    <q-separator spaced='md' />
+    <q-btn color='black' class='q-mb-lg' label='new Configuration ...' />
+    <br />
+    <q-btn color='black' class='q-mb-lg' label='Open File ...' />
+    <br />
+    <q-btn color='black' class='q-mb-lg' label='Save' />
+    <br />
+    <q-btn color='black' class='q-mb-lg' label='Save As...' />
+    <br />
+    <q-btn color='black' class='q-mb-lg' label='Close' />
+    <br />
+    <q-btn color='black' class='q-mb-lg' label='Exit' />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreferenceFile',
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
